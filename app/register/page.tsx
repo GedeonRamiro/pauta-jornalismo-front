d37d6515f416dd14c2fa84768347684d5b3d4b99
Gedeon <gedeon_ramiro@hotmail.com>
2025-10-17 16:40:21 -0300

    feat: component Sidebar

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,5 @@
 import CreateAccount from "../components/Account/CreateAccount";
-import Start from "../components/Start";
+import Start from "../components/Account/InfoAccount";
 
 export default function Register() {
   return <Start accountProps={<CreateAccount />} />;
