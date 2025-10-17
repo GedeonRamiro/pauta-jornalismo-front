612b67302cb23fb7a599cb76cfc7db90a0177844
Gedeon <gedeon_ramiro@hotmail.com>
2025-10-17 16:35:09 -0300

    feat: ToastContainer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.variable} ${poppins.variable} antialiased`}>
+        <ToastContainer />
         {children}
       </body>
     </html>
